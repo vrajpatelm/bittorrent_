6219d54f193d05da85785c7687475795f6338e6f
Vraj0Hub <vrajpatel.01810@gmail.com>
2026-02-02 19:44:47 +0530

    The parsing functionality is completed fully

--- parser.py
+++ parser.py
@@ -1,10 +1,12 @@
 import sys
 import os
 import bencodepy
+from numpy import byte
+
 
 # def read_torrent_file(torrent_file):
 # def encode_to_bencode(data):
-def encode_to_bencode(info_file):
+def encode_to_dict(info_file):
     def parse_str(data1,item=0):
         colon = data1.index(b":", item)
         len_str = int(data1[item:colon])
@@ -56,9 +58,51 @@ def encode_to_bencode(info_file):
 
 
 
+# def dict_to_bencode_dict(data,item=0):
+#     dict_to_bencode_dict(data)
+def dict_to_bencode_dict_(data1):
+    str1 =b""
+    for i in data1.items():
+        str1 = str1 + str_to_bencode(i[0])
+        str1 = str1 + find_parse(i[1])
+    return b"d"+str1+b"e"
 
 
-print(encode_to_bencode("C:\\Users\VRAJ\Downloads\PG hostel.txt.torrent")[b"info"])
+def str_to_bencode(value ):
+    return  b""+ str(len(value)).encode() + b":"+value
+
+
+def byte_to_bencode(value):
+    return b"i"+str(value).encode()+b"e"
+
+
+def find_parse(value ):
+    if type(value) == str:
+        return str_to_bencode(value.encode() )
+    elif type(value) == bytes:
+        return str_to_bencode(value)
+    elif type(value) == int:
+        return byte_to_bencode(value)
+    elif type(value) == list:
+        ls1 = b"l"
+        for i in range(len(value)):
+          ls1 += find_parse(value[i])
+        return ls1+b"e"
+    elif type(value) == dict:
+        return dict_to_bencode_dict_(value )
+    else:
+        raise ValueError(f"Invalid input {value}")
+
+
+
+
+encoded_dict=encode_to_dict("C:\\Users\VRAJ\Downloads\PG hostel.txt.torrent")
+print(encoded_dict)
+print(open("C:\\Users\VRAJ\Downloads\PG hostel.txt.torrent","rb").read())
+print("\n")
+
+print(find_parse(encoded_dict))
+# print(dict_to_bencode_dict(encoded_dict))
 
 
 
